c913105bfeef8976a9a6d9940564a4ad1cd8e7df
julian-blohm <julian@startup-werk.de>
2024-08-25 00:57:26 +0200

    add bitwise operators and make console output more readable

--- 06_operators/main.go
+++ 06_operators/main.go
@@ -14,13 +14,19 @@ func main() {
 	 */
 	var name1 string = "bob"
 	var name2 string = "joe"
+	fmt.Print("equal: ")
 	fmt.Println(name1 == name2)
+	fmt.Print("not equal: ")
 	fmt.Println(name1 != name2)
 
 	var number1, number2 = 2, 3
+	fmt.Print("less than: ")
 	fmt.Println(number1 < number2)
+	fmt.Print("less or equal: ")
 	fmt.Println(number1 <= number2)
+	fmt.Print("greater than: ")
 	fmt.Println(number1 > number2)
+	fmt.Print("greater or equal: ")
 	fmt.Println(number1 >= number2)
 
 	/*
@@ -34,15 +40,22 @@ func main() {
 	* decrement: -- ; unary operator, does not need two operands
 	 */
 
+	fmt.Print("addition: ")
 	fmt.Println(name1 + name2)
+	fmt.Print("subtraction: ")
 	fmt.Println(number1 - number2)
+	fmt.Print("multiplication: ")
 	fmt.Println(number1 * number2)
+	fmt.Print("division: ")
 	fmt.Println(number1 / number2)
+	fmt.Print("modulus: ")
 	fmt.Println(number1 % number2)
 
 	number1++
+	fmt.Print("increment: ")
 	fmt.Println(number1)
 	number1--
+	fmt.Print("decrement: ")
 	fmt.Println(number1)
 
 	/*
@@ -53,14 +66,16 @@ func main() {
 	 */
 
 	number1 = 10
+	fmt.Println("logical AND: ")
 	fmt.Println((number1 < 100) && (number1 < 200))
 	fmt.Println((number1 < 300) && (number1 < 0))
-	fmt.Println("")
 
+	fmt.Println("logical OR: ")
 	fmt.Println((number1 < 100) || (number1 < 0))
 	fmt.Println((number1 > 100) || (number1 < 0))
 
 	// just for showcasing; sonar doesnt like it
+	fmt.Print("logical NOT: ")
 	fmt.Println(!(true))
 
 	/*
@@ -75,26 +90,54 @@ func main() {
 	number1 = 5
 	number2 = 10
 	number1 = number2
-	fmt.Println(number1)
+	fmt.Println("assign: ", number1)
 
 	number1 = 5
 	number1 += number2 // same as num1 = num1 + num2
-	fmt.Println(number1)
+	fmt.Println("add and assign: ", number1)
 
 	number1 = 5
 	number1 -= number2 // same as num1 = num1 - num2
-	fmt.Println(number1)
+	fmt.Println("subtract and assign: ", number1)
 
 	number1 = 5
 	number1 *= number2 // same as num1 = num1 * num2
-	fmt.Println(number1)
+	fmt.Println("multiply and assign: ", number1)
 
 	number1 = 5
 	number1 /= number2 // same as num1 = num1 / num2
-	fmt.Println(number1)
+	fmt.Println("divide and assign quotient: ", number1)
 
 	number1 = 5
 	number1 %= number2 // same as num1 = num1 % num2 (remainder of division is asigned)
-	fmt.Println(number1)
+	fmt.Println("divide and assign modulus: ", number1)
 
+	/*
+	* Bitwise operators
+	* bitwise and: & ; takes two numbers as operands and does AND on every bit of two numbers
+	* bitwise or: | ; similar bitwise AND but with OR
+	* bitwise xor: ^ ; XOR on every bit of the number
+	* left shift: << ; shift all bits left by specified number
+	* right shift: >>
+	 */
+	// bitwise AND
+	var x, y int = 12, 25 // 00001100 and 00011001 in binary
+	z := x & y            // 8  (00001000) would be the anser
+	fmt.Println("bitwise AND: ", z)
+
+	// bitwise OR
+	z = x | y // 29 (00011101)
+	fmt.Println("bitwise OR: ", z)
+
+	// bitwise XOR
+	z = x ^ y // 21 (00010101)
+	fmt.Println("bitwise XOR: ", z)
+
+	// shift left
+	z = x << 1 // 24 (00011000)
+	fmt.Println("shift left: ", z)
+
+	// shift right
+	z = x >> 2 // 3 (00000011)
+	fmt.Println("shift right: ", z)
 }
