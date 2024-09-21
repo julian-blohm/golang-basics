ef43f9e4a762921622b4479b81a232b003705f1f
julian-blohm <julian@startup-werk.de>
2024-09-21 17:54:39 +0200

    add pass by re and value for maps and slices

--- 13_pointers/main.go
+++ 13_pointers/main.go
@@ -5,19 +5,20 @@ import (
 )
 
 /*
-* pointer is a variable that holds memory adress of another variable
-* also points where memory is allocated; finding and changing values possible
+ * pointer is a variable that holds memory adress of another variable
+ * also points where memory is allocated; finding and changing values possible
  */
 
 func main() {
 
 	/*
-	* address operator
-	* & operator - '&' followed by var needed for 'adress-of operator'
-	*
-	* dereference operator
-	* * operator - '*' followed by var returns value at the address
+	 * address operator
+	 * & operator - '&' followed by var needed for 'adress-of operator'
+	 *
+	 * dereference operator
+	 * * operator - '*' followed by var returns value at the address
 	 */
+	fmt.Println("\naddress and dereference opator:")
 	i := 10
 	fmt.Printf("%T %v \n", &i, &i)
 	fmt.Printf("%T %v \n", *(&i), *(&i))
@@ -26,7 +27,7 @@ func main() {
 	 * declaring an initializing pointers
 	 * var <pointername> *<datatype> = &<variablename>
 	 */
-
+	println("\ndeclaring and initializing pointers:")
 	var ptrStr *string
 	fmt.Println(ptrStr)
 
@@ -36,7 +37,6 @@ func main() {
 	// another way
 	s := "hello"
 	var ptrS = &s
-
 	fmt.Println(ptrS)
 
 	// using shorthand operator
@@ -46,7 +46,62 @@ func main() {
 	fmt.Printf("%T %v \n", &mystring, &mystring)
 
 	// dereferencing a pointer
+	fmt.Println("\ndereference pointer:")
 	*mypointer = "my world"
 	fmt.Printf("%T %v \n", &mystring, &mystring)
 
+	/* passing by value
+	 * pass value of var as an argument
+	 * parameter is copied into another location in memory
+	 * only copy is modified in function, original not
+	 * basic datatype (int, bool, float, string, array) are passed by value
+	 */
+	fmt.Println("\npass by value:")
+	a := "hello"
+	fmt.Println(a)
+	modifyPbV(a)
+	fmt.Println(a)
+
+	/* passing by reference
+	 * pointers allow to pass references to values
+	 * with call by reference/pointer address of var is passed into function call
+	 * as actual parameter
+	 * all operations in the function are performed on the value at the address
+	 * of the actual parameter
+	 */
+	fmt.Println("\npass by reference:")
+	modifyPbR(&a)
+	fmt.Println(a)
+
+	// slices and maps are passed be reference by default
+	fmt.Println("\npass by reference is default for slices and maps:")
+	slice := []int{10, 20, 30}
+	fmt.Println(slice)
+	modifySlice(slice)
+	fmt.Println(slice)
+
+	myMap := make(map[string]int)
+	myMap["A"] = 35
+	myMap["B"] = 40
+	fmt.Println(myMap)
+	modifyMap(myMap)
+	fmt.Println(myMap)
+}
+
+// pass by value
+func modifyPbV(str string) {
+	str = "world"
+}
+
+// pass by reference
+func modifyPbR(str *string) {
+	*str = "world"
+}
+
+func modifySlice(s []int) {
+	s[0] = 100
+}
+
+func modifyMap(m map[string]int) {
+	m["K"] = 65
 }
